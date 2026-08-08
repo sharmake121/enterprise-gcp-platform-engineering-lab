7f16de375bc9207ab9574760d09969ce1ada9944
sharmake121 <your-email@example.com>
2026-08-08 12:09:32 +0300

    feat: grant terraform service account required roles

--- terraform/modules/service-account/main.tf
+++ terraform/modules/service-account/main.tf
@@ -1,15 +1,28 @@
 resource "google_service_account" "terraform" {
-
   account_id   = var.account_id
   display_name = var.display_name
   project      = var.project_id
 }
 
-resource "google_project_iam_member" "terraform_editor" {
+locals {
+  terraform_roles = [
+    "roles/compute.networkAdmin",
+    "roles/container.admin",
+    "roles/storage.admin",
+    "roles/logging.admin",
+    "roles/monitoring.admin",
+    "roles/serviceusage.serviceUsageAdmin",
+    "roles/iam.serviceAccountAdmin",
+    "roles/iam.serviceAccountUser",
+    "roles/resourcemanager.projectIamAdmin",
+    "roles/accesscontextmanager.policyAdmin"
+  ]
+}
+
+resource "google_project_iam_member" "terraform_roles" {
+  for_each = toset(local.terraform_roles)
 
   project = var.project_id
-  role    = "roles/editor"
-  #role = "roles/resourcemanager.projectIamAdmin"
-
-  member = "serviceAccount:${google_service_account.terraform.email}"
+  role    = each.value
+  member  = "serviceAccount:${google_service_account.terraform.email}"
 }
